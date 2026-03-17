069626c1eed72b1a654ba483a80cf191db2285c9
David24330 <dmorgado699@gmail.com>
2026-03-17 14:52:33 +0100

    sonar fix

--- AdvancedDevSample.Infrastructure/Repositories/EfProductRepository.cs
+++ AdvancedDevSample.Infrastructure/Repositories/EfProductRepository.cs
@@ -13,10 +13,6 @@ namespace AdvancedDevSample.Infrastructure.Repositories
     {
         public Product? GetById(Guid id)
         { // accès base de données
-            //var entity = _dbContext.Products.SingleOrDefault(p => p.Id == id);
-
-            //if (entity == null)
-            //    return null;
             ProductEntity product = new() { Id = id, Price = 10, IsActive = true, Libelle = "" };
             var domainProduct = new Product(id: product.Id, prix: product.Price, isActive: product.IsActive, libelle: product.Libelle);
 
